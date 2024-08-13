60cf55207b2efa00c55b51f39b91e314ffafb6cf
Kevin Lim <kevinlbthai79@gmail.com>
2024-08-13 23:53:55 +0800

    Commit AssertionTest.java

--- src/test/java/DemoTestNG/AssertionTest.java
+++ src/test/java/DemoTestNG/AssertionTest.java
@@ -44,15 +44,9 @@ public class AssertionTest {
 
     @Test
     public void testRadioButtons() {
-        driver.findElement(By.linkText("Radio Buttons Demo")).click();
-        driver.findElement(By.cssSelector("input[value='Other']")).click();
-        driver.findElement(By.xpath("//input[@value='5 - 15']")).click();
-        driver.findElement(By.xpath("//button[text()='Get values']")).click();
-        String actualGender =
-                driver.findElement(By.cssSelector(".genderbutton")).getText();
-
-        String actualAgeGroup =
-                driver.findElement(By.cssSelector(".groupradiobutton")).getText();
+        findAllRadioButton(driver);
+        String actualGender = clickGender(driver);
+        String actualAgeGroup = findAgeGroup(driver);
 
         softAssert.assertEquals(actualGender, "Male",
                 "\n Actual Gender Is Not Correct \n");
@@ -61,5 +55,20 @@ public class AssertionTest {
                 "\n Actual Age Group Is Not Correct \n");
 
         softAssert.assertAll("\n Test Soft Assert");
+    }
+
+    private String findAgeGroup(WebDriver driver) {
+        return driver.findElement(By.cssSelector(".groupradiobutton")).getText();
+    }
+
+    private String clickGender(WebDriver driver) {
+        return driver.findElement(By.cssSelector(".genderbutton")).getText();
+    }
+
+    private void findAllRadioButton(WebDriver driver) {
+        driver.findElement(By.linkText("Radio Buttons Demo")).click();
+        driver.findElement(By.cssSelector("input[value='Other']")).click();
+        driver.findElement(By.xpath("//input[@value='5 - 15']")).click();
+        driver.findElement(By.xpath("//button[text()='Get values']")).click();
     }
 }
