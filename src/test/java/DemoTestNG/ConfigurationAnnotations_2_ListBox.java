7a4df153e80cb1df3222654b15c7d64ed0a37829
Kevin Lim <kevinlbthai79@gmail.com>
2024-08-13 16:33:53 +0800

    initial commit to tutorial TestNG

--- src/test/java/DemoTestNG/ConfigurationAnnotations_2_ListBox.java
+++ src/test/java/DemoTestNG/ConfigurationAnnotations_2_ListBox.java
@@ -1,2 +1,65 @@
-package DemoTestNG;public class ConfigurationAnnotations_2_ListBox {
+package DemoTestNG;
+
+import org.testng.annotations.*;
+
+@Test(groups = "smoke")
+public class ConfigurationAnnotations_2_ListBox {
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("    (4) Execute Before Each Test Method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("    (4) Execute After Each Test Method \n");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("   (3) Execute Before Class: List Box");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("   (3) Execute After Class: List Box \n");
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("  (2) Execute Before Each Test");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("  (2) Execute After Each Test");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println(" (1) Execute Before The Suite");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println(" (1) Execute After The Suite");
+    }
+
+    public void test3_BootstrapListBox() {
+        System.out.println("     (5) Test Method 3: Bootstrap List Box");
+    }
+
+    public void test4_JQueryListBox() {
+        System.out.println("     (5) Test Method 4: JQuery List Box");
+    }
+
+    @BeforeGroups(groups = {"smoke", "regression"})
+    public  void beforeGroups() {
+        System.out.println("Execute Before Group:");
+    }
+
+    @AfterGroups(groups = {"smoke", "regression"})
+    public  void afterGroups() {
+        System.out.println("Execute After Group:");
+    }
 }
--- src/test/java/DemoTestNG/First_Automated_Test.java
+++ src/test/java/DemoTestNG/First_Automated_Test.java
@@ -1,2 +1,41 @@
-package DemoTestNG;public class First_Automated_Test {
+package DemoTestNG;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import static org.openqa.selenium.By.*;
+
+public class First_Automated_Test {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver  = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.lambdatest.com/selenium-playground/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test(priority = 1)
+    public void testTableSortAndSearch() {
+        driver.findElement(By.linkText("Table Sort & Search")).click();
+        driver.findElement(
+                By.xpath("//div[@id='example_filter']//input[@type='search']"))
+                  .sendKeys("J. Caldwell");
+    }
+
+    @Test(priority = 2)
+    public void testBootstrapDatePicker() {
+        driver.findElement(By.linkText("Bootstrap Date Picker")).click();
+        driver.findElement(By.id("birthday")).sendKeys("06/07/2008");
+    }
 }
